69ab26ba5f9719d797d9b56dde0726fbea77f6b7
IpsumCapra <samuelcornelisse@gmail.com>
2022-06-13 11:50:22 +0200

    Added manual controller code.

--- main.py
+++ main.py
@@ -1,144 +1,72 @@
 import time as tm
-import traceback as tb
-import math as mt
+import socket_wrapper as sw
 import sys as ss
 import os
 import socket as sc
+import pygame
 
 ss.path += [os.path.abspath(relPath) for relPath in ('..',)]
 
-finity = 20.0   # Needs to be float to obtain ditto numpy array
+finity = 20.0  # Needs to be float to obtain ditto numpy array
 
 lidarInputDim = 16
 sonarInputDim = 3
 
 sampleFileName = 'default.samples'
 
-def getTargetVelocity (steeringAngle):
-    return (90 - abs (steeringAngle)) / 60
 
-import socket_wrapper as sw
+def getTargetVelocity(steeringAngle):
+    return (90 - abs(steeringAngle)) / 60
 
+pygame.init()
+display = pygame.display.set_mode((300, 300))
 
-class HardcodedClient:
+angleStep = 1
+
+class ManualControl:
     def __init__(self):
         self.steeringAngle = 0
+        self.brake = True
 
         with open(sampleFileName, 'w') as self.sampleFile:
             with sc.socket(*sw.socketType) as self.clientSocket:
                 self.clientSocket.connect(sw.address)
                 self.socketWrapper = sw.SocketWrapper(self.clientSocket)
-                self.halfApertureAngle = False
 
                 while True:
-                    self.input()
-                    self.sweep()
-                    self.output()
-                    self.logTraining()
-                    tm.sleep(0.02)
+                    event = pygame.event.get()
+                    self.output(event)
+                    self.logLidarTraining()
+                    tm.sleep(0.04)
 
-    def input(self):
-        sensors = self.socketWrapper.recv()
+    def output(self, event):
+        keys = pygame.key.get_pressed()
 
-        if not self.halfApertureAngle:
-            self.halfApertureAngle = sensors['halfApertureAngle']
-            self.sectorAngle = 2 * self.halfApertureAngle / lidarInputDim
-            self.halfMiddleApertureAngle = sensors['halfMiddleApertureAngle']
+        # Go left.
+        if keys[pygame.K_a]:
+            self.steeringAngle = max(self.steeringAngle + angleStep, -90)
+        # Go right.
+        elif keys[pygame.K_d]:
+            self.steeringAngle = min(self.steeringAngle - angleStep, 90)
+        # else:
+        #     if self.steeringAngle > 0:
+        #         self.steeringAngle -= angleStep
+        #     elif self.steeringAngle < 0:
+        #         self.steeringAngle += angleStep
 
-        if 'lidarDistances' in sensors:
-            self.lidarDistances = sensors['lidarDistances']
-        else:
-            self.sonarDistances = sensors['sonarDistances']
+        for e in event:
+            if e.type == pygame.KEYDOWN and keys[pygame.K_TAB]:
+                self.brake = not self.brake
 
-    def lidarSweep(self):
-        nearestObstacleDistance = finity
-        nearestObstacleAngle = 0
-
-        nextObstacleDistance = finity
-        nextObstacleAngle = 0
-
-        for lidarAngle in range(-self.halfApertureAngle, self.halfApertureAngle):
-            lidarDistance = self.lidarDistances[lidarAngle]
-
-            if lidarDistance < nearestObstacleDistance:
-                nextObstacleDistance = nearestObstacleDistance
-                nextObstacleAngle = nearestObstacleAngle
-
-                nearestObstacleDistance = lidarDistance
-                nearestObstacleAngle = lidarAngle
-
-            elif lidarDistance < nextObstacleDistance:
-                nextObstacleDistance = lidarDistance
-                nextObstacleAngle = lidarAngle
-
-        targetObstacleDistance = (nearestObstacleDistance + nextObstacleDistance) / 2
-
-        self.steeringAngle = (nearestObstacleAngle + nextObstacleAngle) / 2
-        self.targetVelocity = getTargetVelocity(self.steeringAngle)
-
-    def sonarSweep(self):
-        obstacleDistances = [finity for sectorIndex in range(3)]
-        obstacleAngles = [0 for sectorIndex in range(3)]
-
-        for sectorIndex in (-1, 0, 1):
-            sonarDistance = self.sonarDistances[sectorIndex]
-            sonarAngle = 2 * self.halfMiddleApertureAngle * sectorIndex
-
-            if sonarDistance < obstacleDistances[sectorIndex]:
-                obstacleDistances[sectorIndex] = sonarDistance
-                obstacleAngles[sectorIndex] = sonarAngle
-
-        if obstacleDistances[-1] > obstacleDistances[0]:
-            leftIndex = -1
-        else:
-            leftIndex = 0
-
-        if obstacleDistances[1] > obstacleDistances[0]:
-            rightIndex = 1
-        else:
-            rightIndex = 0
-
-        self.steeringAngle = (obstacleAngles[leftIndex] + obstacleAngles[rightIndex]) / 2
-        self.targetVelocity = getTargetVelocity(self.steeringAngle)
-
-    def sweep(self):
-        if hasattr(self, 'lidarDistances'):
-            self.lidarSweep()
-        else:
-            self.sonarSweep()
-
-    def output(self):
         actuators = {
             'steeringAngle': self.steeringAngle,
-            'targetVelocity': self.targetVelocity
+            'targetVelocity': 0.5 if not self.brake else 0
         }
 
         self.socketWrapper.send(actuators)
 
     def logLidarTraining(self):
-        sample = [finity for entryIndex in range(lidarInputDim + 1)]
-
-        for lidarAngle in range(-self.halfApertureAngle, self.halfApertureAngle):
-            sectorIndex = round(lidarAngle / self.sectorAngle)
-            sample[sectorIndex] = min(sample[sectorIndex], self.lidarDistances[lidarAngle])
-
-        sample[-1] = self.steeringAngle
-        print(*sample, file=self.sampleFile)
-
-    def logSonarTraining(self):
-        sample = [finity for entryIndex in range(sonarInputDim + 1)]
-
-        for entryIndex, sectorIndex in ((2, -1), (0, 0), (1, 1)):
-            sample[entryIndex] = self.sonarDistances[sectorIndex]
-
-        sample[-1] = self.steeringAngle
-        print(*sample, file=self.sampleFile)
-
-    def logTraining(self):
-        if hasattr(self, 'lidarDistances'):
-            self.logLidarTraining()
-        else:
-            self.logSonarTraining()
+        pass
 
 
-HardcodedClient()
+ManualControl()
