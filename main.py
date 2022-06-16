174443e5773958b2f96d39885030f6997c506e75
IpsumCapra <samuelcornelisse@gmail.com>
2022-06-16 12:43:48 +0200

    Altered regressor settings.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ lidarInputDim = 16
 
 sampleFileName = 'datasets/default.samples'
 
-X = np.loadtxt("datasets/dataSet", delimiter=' ')
+X = np.loadtxt("datasets/default.samples", delimiter=' ')
 
 modelSaveFile = 'model.sav'
 
@@ -25,30 +25,37 @@ class AIClient:
     def __init__(self):
         self.steeringAngle = 0
 
-        createModel = input("Create new model? [y/N]")
+        createModel = input("Create new model? [y/N]: ")
 
         if 'y' not in createModel.lower():
             try:
                 self.neuralNet = pickle.load(open(modelSaveFile, 'rb'))
             except Exception:
                 raise FileNotFoundError
+            print("Loaded model.")
         else:
-            self.neuralNet = MLPRegressor(random_state=1, max_iter=100000)
+            print("Training...")
+            self.neuralNet = MLPRegressor(learning_rate_init=0.005,
+                                          n_iter_no_change=200,
+                                          verbose=True,
+                                          random_state=1,
+                                          hidden_layer_sizes=(128, 128, 128),
+                                          max_iter=100000)
             self.neuralNet.fit(X[:, :-1], X[:, -1])
+            print(self.neuralNet.best_loss_)
             pickle.dump(self.neuralNet, open(modelSaveFile, 'wb'))
             print(f"Training finished in {self.neuralNet.n_iter_} cycles.")
 
-        with open(sampleFileName, 'w') as self.sampleFile:
-            with sc.socket(*sw.socketType) as self.clientSocket:
-                self.clientSocket.connect(sw.address)
-                self.socketWrapper = sw.SocketWrapper(self.clientSocket)
-                self.halfApertureAngle = False
+        with sc.socket(*sw.socketType) as self.clientSocket:
+            self.clientSocket.connect(sw.address)
+            self.socketWrapper = sw.SocketWrapper(self.clientSocket)
+            self.halfApertureAngle = False
 
-                while True:
-                    self.input()
-                    self.lidarSweep()
-                    self.output()
-                    tm.sleep(0.02)
+            while True:
+                self.input()
+                self.lidarSweep()
+                self.output()
+                tm.sleep(0.02)
 
     def input(self):
         sensors = self.socketWrapper.recv()
@@ -61,28 +68,6 @@ class AIClient:
         self.lidarDistances = sensors['lidarDistances']
 
     def lidarSweep(self):
-        nearestObstacleDistance = finity
-        nearestObstacleAngle = 0
-
-        nextObstacleDistance = finity
-        nextObstacleAngle = 0
-
-        for lidarAngle in range(-self.halfApertureAngle, self.halfApertureAngle):
-            lidarDistance = self.lidarDistances[lidarAngle]
-
-            if lidarDistance < nearestObstacleDistance:
-                nextObstacleDistance = nearestObstacleDistance
-                nextObstacleAngle = nearestObstacleAngle
-
-                nearestObstacleDistance = lidarDistance
-                nearestObstacleAngle = lidarAngle
-
-            elif lidarDistance < nextObstacleDistance:
-                nextObstacleDistance = lidarDistance
-                nextObstacleAngle = lidarAngle
-
-        targetObstacleDistance = (nearestObstacleDistance + nextObstacleDistance) / 2
-
         sample = [finity for eI in range(lidarInputDim)]
         for lidarAngle in range(-self.halfApertureAngle, self.halfApertureAngle):
             sectorIndex = round(lidarAngle / self.sectorAngle)
@@ -91,13 +76,12 @@ class AIClient:
         numpySample = np.array(sample).reshape(1, -1)
 
         self.steeringAngle = self.neuralNet.predict(numpySample)[0]
-        print(self.steeringAngle)
+
         self.targetVelocity = getTargetVelocity(self.steeringAngle)
 
     def output(self):
-        print(self.steeringAngle)
         actuators = {
-            'steeringAngle': self.steeringAngle * 2,
+            'steeringAngle': self.steeringAngle,
             'targetVelocity': self.targetVelocity
         }
 
